bcbfb0838feadf9af56d65dd03a1ae390fec109a
aamsayem <aamsayem01@gmail.com>
2026-05-04 00:30:25 +0600

    Fix GitHub Pages root deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
   base: "./",
+  build: {
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, "app.html"),
+      },
+    },
+  },
   server: {
     host: "::",
     port: 8080,
